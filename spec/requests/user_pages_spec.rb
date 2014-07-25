9f519f6cd7fd2a783481cf714c90df42ccae0fb5
Victor E. Leshkov <mast3rrr@gmail.com>
2014-07-25 08:00:29 +0000

    Done excercises from 7th chapter

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -16,7 +16,7 @@ describe "UserPages" do
 
 
 
-	describe "signup page" do
+	describe "signup" do
 
     before { visit signup_path }
 
@@ -25,6 +25,13 @@ describe "UserPages" do
     describe "with invalid information" do
       it "should not create a user" do
         expect { click_button submit }.not_to change(User, :count)
+      end
+
+      describe "after submission" do
+        before { click_button submit }
+
+        it { should have_title('Sign up') }
+        it { should have_content('error') }
       end
     end
 
@@ -38,6 +45,14 @@ describe "UserPages" do
 
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
+      end
+
+      describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by(email: 'user@example.com') }
+
+        it { should have_title(user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
       end
     end
   end
